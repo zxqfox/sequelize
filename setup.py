48d8fb4a4266ed5559e74d4e72cbe1f4e49e28c7
Jan Aagaard Meier <janzeh@gmail.com>
2015-05-17 14:36:56 +0200

    .

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-print 'wat'
+
+def get_git_revision_hash():
+    import subprocess
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+
+
+with open('docs/api/instance.md', 'r') as f:
+    s = f.read()
+
+    s = s.replace('{{HASH}}', get_git_revision_hash())
+
+with open('docs/api/instance.md', 'w+') as f:
+    f.write(s)
